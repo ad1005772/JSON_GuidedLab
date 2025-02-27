b4797d12aa905f440bf2f403c477ccc56c93a058
ad1005772 <ad1005772@otc.edu>
2025-02-27 09:09:34 -0600

    Email_isStudent
    Completed the lab and added the Email and isStudent. Just have to make the list and exceptions.

---
+++ JSON_GuidedLab/JSON_GuidedLab/Program.cs
@@ -0,0 +1,23 @@
+﻿using JSON_GuidedLab;
+using Newtonsoft.Json;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person
+        {
+            Name = "John Doe",
+            Age = 30,
+            Email = "johndoe@gmail.com",
+            isStudent = true
+        };
+
+        string json = JsonConvert.SerializeObject(person);
+        Console.WriteLine("Serialized JSON: " + json);
+
+        Person deserializedPerson = JsonConvert.DeserializeObject<Person>(json);
+        Console.WriteLine("Deserialized Person: Name - " + deserializedPerson.Name + ", Age - " + deserializedPerson.Age + ", Email - " + deserializedPerson.Email + ", Are they a student? - " + deserializedPerson.isStudent);
+    }
+}
